00466d06d48b7500a1123bff4e4c37ad175defac
Gabor Grosschmid <gabor.grosschmid@ppt.eu>
2013-03-15 17:38:52 +0100

    nexmo plugin added

---
+++ application/logs/2013-03-15.log.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2013-03-15 15:56:16 +00:00 --- error: Missing i18n entry ui_main.api_logs for language en_US
+2013-03-15 15:56:18 +00:00 --- error: Missing i18n entry ui_main.api_banned for language en_US
+2013-03-15 15:56:18 +00:00 --- error: core.uncaught_exception
+2013-03-15 15:56:23 +00:00 --- error: Missing i18n entry ui_main.api_logs for language en_US
